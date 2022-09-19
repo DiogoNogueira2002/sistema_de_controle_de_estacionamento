755bc50d7290f32944588b4f8cfd49302ccfd9bb
DiogoNogueira2002 <estudantediogo2203@gmail.com>
2022-09-18 23:51:07 -0300

    0.4

---
+++ principal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "telas.h"
+
+
+int main(void)
+{
+    tela_principal();
+    tela_clientes();
+    tela_estac();
+    tela_pag();
+    tela_precos();
+    tela_sobre();
+    return 0;
+}

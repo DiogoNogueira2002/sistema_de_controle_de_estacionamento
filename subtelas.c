cf5aee39604eb855f964186f1b870dda9f48c231
Diogo Víctor <104800101+DiogoNogueira2002@users.noreply.github.com>
2022-09-26 13:27:38 -0300

    Update subtelas.c

--- subtelas.c
+++ subtelas.c
@@ -4,12 +4,9 @@
 void subtela_cadastrarcliente(void) {
   // no momento do cadastro ele armazenará o tempo que foi realizado
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========                   Cadastrar Cliente                 "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========                   Cadastrar Cliente                 =========\n");
+  printf("=======================================================================\n");
   printf("========= Nome: Diogo Victor\n");
   printf("========= Vaga do Estacionamento: 001\n");
   printf("========= Tipo do Veículo: Carro\n");
@@ -24,12 +21,9 @@ void subtela_mostrarclientes(void) {
   // quando mostrar o relatório, pegará novamente o tempo atual e checará com o
   // tempo de cadastro, verificando o tempo restante.
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========             Mostrar Todos os Clientes               "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========             Mostrar Todos os Clientes               =========\n");
+  printf("=======================================================================\n");
   printf("Nome: Diogo Victor\n");
   printf("Vaga do Estacionamento: 001\n");
   printf("Tipo do Veículo: Carro\n");
@@ -46,12 +40,9 @@ char subtela_editarcliente(void) {
   // no momento do cadastro ele armazenará o tempo que foi realizado
   char opcao;
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========                     Editar Cliente                  "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========                     Editar Cliente                  =========\n");
+  printf("=======================================================================\n");
   printf("========= Informe a Placa do Veículo: MHZ9889\n");
   printf("=========\n");
   printf("========= 1 - Nome: Diogo Victor\n");
@@ -73,12 +64,9 @@ char subtela_removercliente(void) {
   // no momento do cadastro ele armazenará o tempo que foi realizado
   char opcao;
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========                     Remover Cliente                 "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========                     Remover Cliente                 =========\n");
+  printf("=======================================================================\n");
   printf("========= Informe a Placa do Veículo: MHZ9889\n");
   printf("=========\n");
   printf("========= 1 - Nome: Diogo Victor\n");
@@ -99,12 +87,9 @@ char subtela_removercliente(void) {
 char subtela_adicionarvagas_esc(void) {
   char opcao;
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========                    Adicionar Vagas                 "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========                    Adicionar Vagas                 =========\n");
+  printf("=======================================================================\n");
   printf("========= Digite 0 para o modo Manual ou 1 para o modo Automático: ");
   scanf("%c", &opcao);
   getchar();
@@ -115,35 +100,26 @@ char subtela_adicionarvagas_esc(void) {
 
 void subtela_adicionarvaga_man(void) {
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========              Adicionar Vagas (Manual)               "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========              Adicionar Vagas (Manual)               =========\n");
+  printf("=======================================================================\n");
   printf("========= \n");
   printf("========= Digite o Nome da Vaga (xxx) ou 000 para voltar: ");
 }
 
 void subtela_adicionarvaga_aut(void) {
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========            Adicionar Vagas (Automático)             "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========            Adicionar Vagas (Automático)             =========\n");
+  printf("=======================================================================\n");
   printf("========= Insira a quantidade de vagas (xxx): ");
 }
 
 void subtela_mostrarvagas(void) {
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========                 Mostrar Todas as Vagas              "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========                 Mostrar Todas as Vagas              =========\n");
+  printf("=======================================================================\n");
   printf("Nome da Vaga: 001\n");
   printf("Estado Atual: Ocupada\n");
   printf("\n");
@@ -155,12 +131,9 @@ void subtela_mostrarvagas(void) {
 
 void subtela_editarvagas(void) {
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========                      Editar Vagas                   "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========                      Editar Vagas                   =========\n");
+  printf("=======================================================================\n");
   printf("========= Informe o Nome da Vaga: 001 \n");
   printf("=========\n");
   printf("========= Nome da Vaga: 001\n");
@@ -172,12 +145,9 @@ void subtela_editarvagas(void) {
 char subtela_removervagas(void) {
   char opcao;
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========                     Remover Vagas                   "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========                     Remover Vagas                   =========\n");
+  printf("=======================================================================\n");
   printf("========= Informe o Nome da Vaga: 001\n");
   printf("=========\n");
   printf("========= Nome da Vaga: 001\n");
@@ -193,12 +163,9 @@ char subtela_removervagas(void) {
 
 void subtela_realizarpag(void) {
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========                 Realizar Pagamento                  "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========                 Realizar Pagamento                  =========\n");
+  printf("=======================================================================\n");
   printf("========= Informe a Placa do Veículo: MHZ9889\n");
   printf("=========\n");
   printf("========= Nome: Diogo Victor\n");
@@ -208,18 +175,14 @@ void subtela_realizarpag(void) {
   printf("========= Tempo de Uso: 30 minutos\n");
   printf("========= Pagamento: Não Realizado\n");
   printf("=========\n");
-  printf(
-      "========= Digite o valor do pagamento (x.xxx) ou 0000 para cancelar:");
+  printf("========= Digite o valor do pagamento (x.xxx) ou 0000 para cancelar:");
 }
 
 void subtela_mostrarpagamentos(void) {
   system("clear||cls");
-  printf("====================================================================="
-         "==\n");
-  printf("=========                   Mostrar Pagamentos                "
-         "=========\n");
-  printf("====================================================================="
-         "==\n");
+  printf("=======================================================================\n");
+  printf("=========                   Mostrar Pagamentos                =========\n");
+  printf("=======================================================================\n");
   printf("Nome: Diogo Victor\n");
   printf("Vaga do Estacionamento: 001\n");
   printf("Tipo do Veículo: Carro\n");
